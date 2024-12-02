8fe070579b2d54e562ca91e0dc4bb9f972f827c2
Gabriel Alerrandro <gabrielalerrandro081@gmail.com>
2024-12-01 22:13:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,148 @@
+import { useEffect } from "react"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import { useReducer } from "react"
+import Loader from "./components/Loader"
+import Error from "./components/Error"
+import StartScreen from "./components/StartScreen"
+import Question from "./components/Question"
+import NextButton from "./components/NextButton"
+import Progress from "./components/Progress"
+import Finished from "./components/Finished"
+import Footer from "./components/Footer"
+import Timer from "./components/Timer"
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+  secondsRemaining: null,
+}
+const SECONDS_PER_QUESTION = 30
+
+function reducer(state, action) {
+  // "loading", "error", "ready","active", "finish"
+
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" }
+
+    case "dataFailed":
+      return { ...state, status: "error" }
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECONDS_PER_QUESTION,
+      }
+    case "newAnswer":
+      const question = state.questions.at(state.index)
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      }
+
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null }
+
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      }
+
+    case "reset":
+      return { ...initialState, questions: state.questions, status: "ready" }
+
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      }
+
+    default:
+      throw new Error("Action unknown")
+  }
+}
+
+function App() {
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState)
+
+  const numQuestions = questions.length
+
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  )
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch({ type: "dataFailed" })
+  }, [])
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              numQuestions={numQuestions}
+              index={index}
+            />
+
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+            </Footer>
+          </>
+        )}
+
+        {status === "finished" && (
+          <Finished
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
+        )}
+      </Main>
+    </div>
+  )
+}
+
+export default App
